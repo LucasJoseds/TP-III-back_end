0b00392494a7e057d866db22af9e7291de6df615
Lucas Jose da Silva <07055338166@tocantins.governo>
2024-06-06 14:26:21 -0300

    .

--- restaurante_back_end/pedidos_back_end/pedidos_back_end/obj/Debug/net8.0/pedidos_back_end.AssemblyInfo.cs
+++ restaurante_back_end/pedidos_back_end/pedidos_back_end/obj/Debug/net8.0/pedidos_back_end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pedidos_back_end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d3d5eb99e67f9bb4c05206b812d9457343b87f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f4c6e2abfacc832ca8a3cf4dc381c0237cc342")]
 [assembly: System.Reflection.AssemblyProductAttribute("pedidos_back_end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pedidos_back_end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
